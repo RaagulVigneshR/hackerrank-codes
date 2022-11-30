f96f1dba35152cc903997bb3662d68dd4c66222e
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-11-30 21:58:37 +0530

    Update objects.js

--- 7 Days of JavaScript/DAY 2/objects.js
+++ 7 Days of JavaScript/DAY 2/objects.js
@@ -1,1 +1,19 @@
+function printObjectProperty(myObject) {
+  console.log(myObject)
+}
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
 
+process.stdin.on("end", function () {
+    var obj = _input.split(' ');
+    var myObject = new Object;
+    myObject.type = "Fiat";
+    myObject.model = "500";
+    myObject.color = "White";
+
+    printObjectProperty(myObject);
+});
