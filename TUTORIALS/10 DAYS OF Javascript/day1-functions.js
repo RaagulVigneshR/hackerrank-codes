5cad53377bebc862047099b78f6d20f24708e49c
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-12-01 09:08:54 +0530

    Create day1-functions.js

---
+++ TUTORIALS/10 DAYS OF Javascript/day1-functions.js
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function factorial(n){
+    let m=1;
+    for(let i=1;i<=n;i++){
+        m*=i;
+    }
+
+    return m;
+}
+
+
+
