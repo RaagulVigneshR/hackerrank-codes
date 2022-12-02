32d49d85f98267a7575ea637e14a01dff60bc425
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-12-02 17:56:09 +0530

    Create day3-arrays.js

---
+++ TUTORIALS/10 DAYS OF Javascript/day3-arrays.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getSecondLargest(nums) {
+    const sorted_array          = nums.sort((a,b) => a - b );
+    const unique_sorted_array   = [...new Set(sorted_array)];
+    return unique_sorted_array[unique_sorted_array.length - 2];
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
