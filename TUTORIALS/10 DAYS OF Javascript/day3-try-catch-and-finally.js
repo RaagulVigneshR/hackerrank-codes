6dcaa36abe477dd0be9fd5a7cdc516fde606101b
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-12-02 17:55:09 +0530

    Create day3-try-catch-and-finally.js

---
+++ TUTORIALS/10 DAYS OF Javascript/day3-try-catch-and-finally.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function reverseString(s) {
+    try {
+        var splitString = s.split("");
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join("");
+        console.log(joinArray);
+    }
+    catch (error)
+        {
+            console.log(error.message);
+            console.log(s);
+        }
+    
+    
+}
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
